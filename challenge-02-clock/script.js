0ada2ff2f4757ad646e0830cd3fcd240743d1e73
Franklin Maria Nayagam <56641239+franklinMN@users.noreply.github.com>
2024-03-16 16:26:10 +0530

    clock completed

---
+++ challenge-02-clock/script.js
@@ -0,0 +1,43 @@
+
+const hourArm = document.querySelector('.hour-arm ');
+const minArm = document.querySelector('.min-arm ');
+const secArm = document.querySelector('.sec-arm ');
+
+function clockMovement(){
+
+    const time = new Date();
+
+    let hours = time.getHours() > 12 ? time.getHours() - 12 : time.getHours();
+    let mins = time.getMinutes();
+    let secs = time.getSeconds();
+
+    console.log(time);
+    // console.log(hours);
+    // console.log(mins);
+    // console.log(secs);
+
+    hours = ( hours / 12 ) * 360 + 90;
+    mins = ( mins / 60 ) * 360 + 90;
+    secs = ( secs / 60 ) * 360 + 90;
+
+    // console.log(hourArm);
+    // console.log(minArm);
+    // console.log(secArm);
+
+    hourArm.style.transform = 'rotate(' + hours + 'deg)';
+    minArm.style.transform = 'rotate(' + mins + 'deg)';
+    secArm.style.transform = 'rotate(' + secs + 'deg)';
+
+    const currentTimeInMilliseconds = time.getTime();
+    const nextSecondInMilliseconds = (Math.floor(currentTimeInMilliseconds / 1000) + 1) * 1000;
+    const delay = nextSecondInMilliseconds - currentTimeInMilliseconds;
+    console.log(currentTimeInMilliseconds, nextSecondInMilliseconds, delay);
+
+    // Call clockMovement at the next second increment
+    setTimeout(clockMovement, delay);
+
+}
+
+clockMovement();
+
+// setInterval(clockMovement, 1000);
